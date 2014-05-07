ea0c7d54e188ef4ac488fd0c05b56c5f6e54c019
7sharp9 <kukulcanenator@googlemail.com>
2014-05-07 19:39:04 +0100

    Added usage sample

---
+++ samples/fullTest/fullTestViewController.fs
@@ -0,0 +1,37 @@
+﻿namespace cstest
+
+open System
+open MonoTouch.UIKit
+open MonoTouch.Foundation
+open Xamarin.iOSProviders
+
+type fstestViewController = Xamarin.iOSProviders.UIProvider<"Main.storyboard">
+type multiController = Xamarin.iOSProviders.UIProvider<"multitest.storyboard">
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit UIApplicationDelegate ()
+
+    let mutable window = new UIWindow (UIScreen.MainScreen.Bounds)
+    let vc = new fstestViewController()
+
+    //override val Window = new UIWindow (UIScreen.MainScreen.Bounds) with get,set
+        //with get() = window
+        //and set v = window <- v
+
+    override this.FinishedLaunching (app, options) =
+        
+        //vc.OnClickUp <-
+        //    fun _ -> printfn "Hello"
+        vc.View.BackgroundColor <- UIColor.Red
+        window.RootViewController <- vc
+        window.MakeKeyAndVisible ()
+        true
+
+
+module Main =
+    [<EntryPoint>]
+    let main args =
+        UIApplication.Main (args, null, "AppDelegate")
+        0
+
