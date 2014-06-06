d8fb7342beeebc8db4f6ef881a1a142aa8f1edae
7sharp9 <kukulcanenator@googlemail.com>
2014-06-06 18:19:36 +0100

    Add comments to example detailing the generated parts

--- samples/fullTest/fullTestViewController.fs
+++ samples/fullTest/fullTestViewController.fs
@@ -5,6 +5,7 @@ open MonoTouch.UIKit
 open MonoTouch.Foundation
 open Xamarin.iOSProviders
 
+//view controller is generated from the type provider and embedded inot the assembly here
 type myViewController = Xamarin.iOSProviders.UIProvider<"../StoryBoards/Main.storyboard">
 
 [<Register ("AppDelegate")>]
@@ -12,9 +13,14 @@ type AppDelegate () =
     inherit UIApplicationDelegate ()
 
     let initialViewController = myViewController.CreateInitialViewController()
+    //this shows a possibility to access the outlets that are generated from the storyboard via the ViewDidLoadAction
+    do initialViewController.ViewDidLoadAction <-
+        fun _ -> initialViewController.myButton.TouchDown.Add (fun _ -> initialViewController.View.BackgroundColor <- UIColor.Blue) 
+
     let window = new UIWindow(UIScreen.MainScreen.Bounds, RootViewController= initialViewController)
 
     override this.FinishedLaunching (app, options) =
+        //This shows a possible way that actions can be assigned via a propery setter that is generated
         initialViewController.Mytouchup <- fun _ -> initialViewController.View.BackgroundColor <- UIColor.Red
         window.MakeKeyAndVisible ()
         true
