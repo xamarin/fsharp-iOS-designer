f4525bf983cd93b9701ff46560d6c636ce7a64d4
7sharp9 <kukulcanenator@googlemail.com>
2014-06-25 11:08:30 +0100

    Add a script scratchpad for experimenting

---
+++ src/Xamarin.iOSProviders/scratchpad.fsx
@@ -0,0 +1,19 @@
+﻿#r "/Developer/MonoTouch/usr/lib/mono/2.1/monotouch.dll"
+open MonoTouch.UIKit
+open System
+open System.Reflection
+
+module TypeExt =
+    type Type with
+        member x.GetVirtualMethods() = 
+            x.GetMethods (BindingFlags.Instance ||| BindingFlags.Public ||| BindingFlags.DeclaredOnly) 
+            |> Array.filter (fun m -> m.IsVirtual)
+    
+let vc = typeof<UIViewController>
+let meths = 
+    vc.GetMethods (BindingFlags.Instance ||| BindingFlags.Public ||| BindingFlags.DeclaredOnly) 
+    |> Array.filter (fun m -> m.IsVirtual && (*not*) (m.ReturnType = typeof<Void>) )
+meths.Length
+
+open TypeExt
+typeof<UIViewController>.GetVirtualMethods() |> Array.map (fun m-> m.Name + " : " + m.ReturnType.FullName )
