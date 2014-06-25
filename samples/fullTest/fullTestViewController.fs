78d9192585019eef403037ab1418b1a78a05a542
7sharp9 <kukulcanenator@googlemail.com>
2014-06-25 11:08:07 +0100

    Add new sample showing inheritance based overriding

--- samples/fullTest/fullTestViewController.fs
+++ samples/fullTest/fullTestViewController.fs
@@ -5,18 +5,36 @@ open MonoTouch.UIKit
 open MonoTouch.Foundation
 open Xamarin.iOSProviders
 
-//view controller is generated from the type provider and embedded inot the assembly here
-type myViewController = Xamarin.iOSProviders.UIProvider<"../StoryBoards/Main.storyboard">
+type StaticHelpers() =
+    static member InstantiateInitialViewController<'a when 'a :> NSObject>( storyboardName) =
+        let mainStoryboard = UIStoryboard.FromName (storyboardName, null)
+        let sb = mainStoryboard.InstantiateInitialViewController ()
+        let theType = sb.GetType()
+        sb :?> 'a
+
+//view controller is generated from the type provider and embedded into the assembly here
+type myViewController = Xamarin.iOSProviders.UIProvider<"../StoryBoards/Main.storyboard", Register = true>
+
+//[<Register("cstestViewController")>]
+//type sub =
+//    inherit myViewController
+//    new ()  = { inherit myViewController() } 
+//    new (handle:nativeint) = { inherit myViewController(handle) } 
+//
+//    override x.ViewDidLoad() =
+//        x.MyButton.TouchDown.Add (fun _ -> x.View.BackgroundColor <- UIColor.Green) 
 
 [<Register ("AppDelegate")>]
 type AppDelegate () =
     inherit UIApplicationDelegate ()
-
+        
     let initialViewController = myViewController.CreateInitialViewController()
+
+    let ivc = StaticHelpers.InstantiateInitialViewController("Main") :?> myViewController
+
     //this shows a possibility to access the outlets that are generated from the storyboard via the ViewDidLoadAction
     do initialViewController.ViewDidLoadAction <-
-        fun _ -> initialViewController.View.ContentMode <- UIViewContentMode.ScaleToFill
-                 initialViewController.MyButton.TouchDown.Add (fun _ -> initialViewController.View.BackgroundColor <- UIColor.Blue) 
+        fun () -> initialViewController.MyButton.TouchDown.Add (fun _ -> initialViewController.View.BackgroundColor <- UIColor.Blue) 
 
     let window = new UIWindow(UIScreen.MainScreen.Bounds, RootViewController= initialViewController)
 
