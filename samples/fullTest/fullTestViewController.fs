7eac18568658c1014e38ba7b3c9c447055b68ce3
7sharp9 <kukulcanenator@googlemail.com>
2014-06-25 20:42:23 +0100

    Remove code cruft

--- samples/fullTest/fullTestViewController.fs
+++ samples/fullTest/fullTestViewController.fs
@@ -5,43 +5,42 @@ open MonoTouch.UIKit
 open MonoTouch.Foundation
 open Xamarin.iOSProviders
 
-type StaticHelpers() =
-    static member InstantiateInitialViewController<'a when 'a :> NSObject>( storyboardName) =
-        let mainStoryboard = UIStoryboard.FromName (storyboardName, null)
-        let sb = mainStoryboard.InstantiateInitialViewController ()
-        let theType = sb.GetType()
-        sb :?> 'a
-
 //view controller is generated from the type provider and embedded into the assembly here
-type myViewController = Xamarin.iOSProviders.UIProvider<"../StoryBoards/Main.storyboard", Register = true>
+//type myViewController = Xamarin.iOSProviders.UIProvider<"../StoryBoards/Main.storyboard", IsRegistered = true>
+type myViewControllerBase = Xamarin.iOSProviders.UIProvider<"../StoryBoards/Main.storyboard", IsRegistered = false, IsAbstract=true>
 
-//[<Register("cstestViewController")>]
-//type sub =
-//    inherit myViewController
-//    new ()  = { inherit myViewController() } 
-//    new (handle:nativeint) = { inherit myViewController(handle) } 
-//
-//    override x.ViewDidLoad() =
-//        x.MyButton.TouchDown.Add (fun _ -> x.View.BackgroundColor <- UIColor.Green) 
+[<Register"cstestViewController">]
+type sub(handle) =
+    inherit myViewControllerBase(handle)
+
+    override x.ViewDidLoad() =
+        x.MyButton.TouchDown.Add (fun _ -> x.View.BackgroundColor <- UIColor.Purple) 
+
+
+    // Return true for supported orientations
+    override x.ShouldAutorotateToInterfaceOrientation (orientation) =
+        orientation <> UIInterfaceOrientation.PortraitUpsideDown
 
 [<Register ("AppDelegate")>]
 type AppDelegate () =
     inherit UIApplicationDelegate ()
-        
-    let initialViewController = myViewController.CreateInitialViewController()
 
-    let ivc = StaticHelpers.InstantiateInitialViewController("Main") :?> myViewController
+//    [<Register("cstestViewController")>]
+//    let initialViewController(a) =
+//        { new myViewControllerBase(a) with 
+//          override x.ViewDidLoad() = x.View.BackgroundColor <- UIColor.Orange}
 
     //this shows a possibility to access the outlets that are generated from the storyboard via the ViewDidLoadAction
-    do initialViewController.ViewDidLoadAction <-
-        fun () -> initialViewController.MyButton.TouchDown.Add (fun _ -> initialViewController.View.BackgroundColor <- UIColor.Blue) 
+    //do initialViewController.ViewDidLoadAction <-
+    //    fun () -> initialViewController.MyButton.TouchDown.Add (fun _ -> initialViewController.View.BackgroundColor <- UIColor.Blue) 
 
-    let window = new UIWindow(UIScreen.MainScreen.Bounds, RootViewController= initialViewController)
+    override val Window = new UIWindow(UIScreen.MainScreen.Bounds) with get,set
 
     override this.FinishedLaunching (app, options) =
+        (this.Window.RootViewController :?> sub).Mytouchup <- fun _ -> this.Window.RootViewController.View.BackgroundColor <- UIColor.Red
         //This shows a possible way that actions can be assigned via a propery setter that is generated
-        initialViewController.Mytouchup <- fun _ -> initialViewController.View.BackgroundColor <- UIColor.Red
-        window.MakeKeyAndVisible ()
+        //initialViewController.Mytouchup <- fun _ -> initialViewController.View.BackgroundColor <- UIColor.Red
+        //window.MakeKeyAndVisible ()
         true
 
 module Main =
