e8c1c16e7635f5788082044500254ea5efb79854
7sharp9 <kukulcanenator@googlemail.com>
2014-06-25 17:00:15 +0100

    Updated script for impromptu code analysis

---
+++ src/Xamarin.iOSProviders/Script.fsx
@@ -0,0 +1,30 @@
+﻿#I "bin/Debug"
+#I "/Developer/MonoTouch/usr/lib/mono/2.1"
+#r "Xamarin.iOSProviders.dll"
+#r "monotouch.dll"
+
+open System
+open System.IO
+open System.Net
+open ProviderImplementation
+open ProviderImplementation.ProvidedTypes
+open Xamarin.iOSProviders
+
+let (++) a b = Path.Combine(a, b)
+let outputFolder = __SOURCE_DIRECTORY__  ++ "bin" ++ "Debug"
+let assemblyName = "iOSDesignerTypeProvider.exe"
+let runtimeAssembly = outputFolder ++ assemblyName
+
+printfn "%s\n%s" outputFolder runtimeAssembly
+let testXib = "/Users/dave/Projects/IOSTypeProviderTests/cstest/MainStoryboard.storyboard"
+let config = [| box testXib; box true|]
+
+let generated = Debug.generate outputFolder runtimeAssembly (fun cfg -> new Xamarin.iOSProviders.iOSDesignerProvider(cfg) :> TypeProviderForNamespaces) config
+
+//These lines build the assembly on disk
+//let assemblyGen = ProvidedAssembly("test.dll")
+//assemblyGen.AddTypes([generated])
+
+let output = Debug.prettyPrint false false 10 100 generated
+printfn "%s" output
+   
