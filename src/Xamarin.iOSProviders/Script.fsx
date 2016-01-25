721a5b30c4e3e4df713c05dc5bf1c7dceebf1437
7sharp9 <dave.thomas@xamarin.com>
2016-01-25 19:17:20 +0000

    Update debug script for latest code changes

--- src/Xamarin.iOSProviders/Script.fsx
+++ src/Xamarin.iOSProviders/Script.fsx
@@ -1,30 +1,33 @@
-﻿#I "bin/Debug"
-#I "/Developer/MonoTouch/usr/lib/mono/2.1"
-#r "Xamarin.iOSProviders.dll"
-#r "monotouch.dll"
-
+﻿#I "/Developer/MonoTouch/usr/lib/mono/2.1"
+#r "/Applications/Xamarin Studio.app/Contents/Resources/lib/monodevelop/AddIns/MonoDevelop.IPhone/MonoTouch.Design.dll"
+#r "/Applications/Xamarin Studio.app/Contents/Resources/lib/monodevelop/AddIns/MonoDevelop.IPhone/MonoTouch.Design.Client.dll"
+#r "packages/ExtCore.0.8.45/lib/net45/ExtCore.dll" 
+#r "/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/Xamarin.iOS.dll"
+#r "System.xml.Linq"
+#load "ProvidedTypes.fs"
+#load "ProvidedTypesHelpers.fs"
+#load "IO.fs"
+#load "Debug.fs"
+#load "Designtime.fs"
+#load "iOSDesignerProvider.fs"
 open System
 open System.IO
 open System.Net
+open System.Linq
 open ProviderImplementation
 open ProviderImplementation.ProvidedTypes
 open Xamarin.iOSProviders
 
 let (/) a b = Path.Combine(a, b)
 let outputFolder = __SOURCE_DIRECTORY__ / "bin" / "Debug"
+let workingFolder = __SOURCE_DIRECTORY__ / "../../samples/StoryBoards/"
 let assemblyName = "iOSDesignerTypeProvider.exe"
 let runtimeAssembly = outputFolder / assemblyName
-
-printfn "%s\n%s" outputFolder runtimeAssembly
-let testXib = "/Users/dave/Projects/IOSTypeProviderTests/cstest/MainStoryboard.storyboard"
-let config = [| box testXib; box true|]
-
-let generated = Debug.generate outputFolder runtimeAssembly (fun cfg -> new Xamarin.iOSProviders.iOSDesignerProvider(cfg) :> TypeProviderForNamespaces) config
+let config = [|box false;box true;box true |]
+let generated = Debug.generate workingFolder runtimeAssembly (fun cfg -> new iOSDesignerProvider(cfg)) config
+let output = Debug.prettyPrint false false 10 100 generated
+printfn "%s" output
 
 //These lines build the assembly on disk
 //let assemblyGen = ProvidedAssembly("test.dll")
 //assemblyGen.AddTypes([generated])
-
-let output = Debug.prettyPrint false false 10 100 generated
-printfn "%s" output
-   
