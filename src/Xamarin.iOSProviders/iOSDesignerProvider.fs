00e0e817f0a0f5c9ba281b9c08361db535305c7b
7sharp9 <dave.thomas@xamarin.com>
2016-03-18 11:53:10 +0000

    refactor provider to use 3 different runtimes and two parsing alternatives

--- src/Xamarin.iOSProviders/iOSDesignerProvider.fs
+++ src/Xamarin.iOSProviders/iOSDesignerProvider.fs
@@ -15,22 +15,8 @@ open ProvidedTypes
 type iOSDesignerProvider(config: TypeProviderConfig) as this = 
     inherit TypeProviderForNamespaces()
 
-    //TODO figure out how to find this properly, must be a env var or something.
-    let baseFolder ="/Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono"
-    
-    let assemblybase, assembly =
-        match Path.GetFileName config.RuntimeAssembly with
-        | a when a = "Xamarin.UIProvider.TVOSRuntime.dll" ->
-            let folder = Path.Combine (baseFolder, "Xamarin.TVOS")
-            let assembly = Path.Combine(folder, "Xamarin.TVOS.dll")
-            folder, Assembly.LoadFrom assembly
-        | a when a = "Xamarin.UIProvider.iOSRuntime.dll" ->
-            let folder = Path.Combine (baseFolder, "Xamarin.iOS")
-            let assembly = Path.Combine(folder, "Xamarin.iOS.dll")
-            folder, Assembly.LoadFrom assembly
-        | other -> failwithf  "Uknown runtime provider assembly: %s" other
-        
-    do this.RegisterProbingFolder assemblybase    
+    let runtimeBinding = RunTime.getRuntimeBinding config.RuntimeAssembly
+    do this.RegisterProbingFolder runtimeBinding.BaseFolder    
 
     let ns = "Xamarin"
     let asm = Assembly.GetExecutingAssembly()
@@ -38,23 +24,26 @@ type iOSDesignerProvider(config: TypeProviderConfig) as this =
     let watchedFiles = ResizeArray()
     let buildTypes typeName (parameterValues: obj []) =
 
-        let allStoryBoards = Directory.EnumerateFiles ( config.ResolutionFolder, "*.storyboard", SearchOption.AllDirectories) 
+        let allStoryBoards = Directory.EnumerateFiles(config.ResolutionFolder, "*.storyboard", SearchOption.AllDirectories) 
         let register = parameterValues.[0] :?> bool
         let isAbstract = parameterValues.[1] :?> bool
         let addUnitCtor = parameterValues.[2] :?> bool
 
         let scenes = 
-            seq { for storyboard in allStoryBoards do
+            seq { for storyboardFile in allStoryBoards do
 
-                    let stream, watcherDisposer = File.openWithWatcher storyboard this.Invalidate
+                    let xdoc, watcherDisposer =
+                        use sss = new StreamReader(storyboardFile, true)
+                        let xdoc = XDocument.Load(sss)
+                        xdoc, File.watch storyboardFile this.Invalidate
+                    
                     watchedFiles.Add watcherDisposer
-                    let xdoc = XDocument.Load(stream)
-                    stream.Close ()
+
                     let scenes = 
-                        match Parser.Instance.Parse(xdoc.Root, DeviceFamily.Undefined) with
-                        | :? Storyboard as sb -> sb.Scenes
-                        | :? Xib as _xib -> failwith "Xib files are currently not supported"
-                        | _ -> failwith "Could not parse file, no supported files were found"
+                        match runtimeBinding.Type with 
+                        | RunTime.MACOS -> Mac.scenesFromXDoc xdoc
+                        | RunTime.IOS 
+                        | RunTime.TVOS -> IOS.scenesFromXDoc xdoc
                     yield! scenes }
 
         let groupedViewControllers = 
@@ -70,7 +59,7 @@ type iOSDesignerProvider(config: TypeProviderConfig) as this =
             [ for sc in groupedViewControllers do
                   let vcs = sc.AsEnumerable()
 //                  if not (String.IsNullOrWhiteSpace sc.ViewController.CustomClass) then
-                  yield TypeBuilder.buildController assembly vcs isAbstract addUnitCtor register config ]
+                  yield TypeBuilder.buildController runtimeBinding vcs isAbstract addUnitCtor register config ]
         
         //Add the types to the container
         container.AddMembers generatedTypes
