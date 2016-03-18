cda666c3bf5dc390314346aa5adf30b1122a984b
7sharp9 <dave.thomas@xamarin.com>
2016-03-18 11:51:33 +0000

    Added generic model describing ios/tvos/macos types

---
+++ src/Xamarin.iOSProviders/Model.fs
@@ -0,0 +1,22 @@
+﻿namespace Xamarin.UIProviders.DesignTime
+open System
+open System.IO
+open System.Xml
+open System.Xml.Linq
+
+type Outlet = {
+    Property:string
+    ElementName: string }
+    
+type Action = {
+    Selector:string
+    ElementName: string }
+               
+type ViewController = {
+    XmlType: string
+    CustomClass : string
+    Outlets: Outlet List
+    Actions: Action List}
+                       
+type Scene = {
+    ViewController : ViewController }
