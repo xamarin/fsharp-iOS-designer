a74823468ae4f71d59771e693be43eca56039445
7sharp9 <dave.thomas@xamarin.com>
2016-03-18 18:57:00 +0000

    Added Xamarin.Mac Cocoa sample with storyboard

---
+++ samples/CocoaApp/AppDelegate.fs
@@ -0,0 +1,16 @@
+﻿namespace CocoaApp
+open System
+open Foundation
+open AppKit
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit NSApplicationDelegate ()
+
+    override x.DidFinishLaunching (notification) =
+        // Insert code here to initialize your application
+        ()
+        
+    override x.WillTerminate(notification) =
+        // Insert code here to tear down your application
+        ()
---
+++ samples/CocoaApp/ViewController.fs
@@ -0,0 +1,20 @@
+﻿namespace CocoaApp
+open System
+open Foundation
+open AppKit
+
+type Container = Xamarin.UIProvider 
+
+[<Register(Container.ViewControllerBase.CustomClass)>] 
+type myViewController(handle) =
+    inherit Container.ViewControllerBase(handle) 
+    
+        override x.ViewDidLoad () =
+            base.ViewDidLoad ()
+            // Do any additional setup after loading the view.
+            //x.OnButton <- Some(fun _ -> x.View.Layer.BackgroundColor <- new CoreGraphics.CGColor("purple"))
+
+        override x.RepresentedObject
+            // Update the view, if already loaded.
+            with get() = base.RepresentedObject
+            and set(v) = base.RepresentedObject <- v
