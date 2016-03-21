fcc2a5e949505107b2371be3401afd5bc8f79713
7sharp9 <dave.thomas@xamarin.com>
2016-03-21 15:50:48 +0000

    Fix iOS parsing of actions and outlets

--- src/Xamarin.iOSProviders/IOSParsing.fs
+++ src/Xamarin.iOSProviders/IOSParsing.fs
@@ -9,6 +9,16 @@ open ExtCore.Control
 open MonoTouch.Design
 
 module IOS =
+    let outletMap (vc:ProxiedViewController) (o:Outlet) = 
+        maybe {
+            let! destination = vc.FindById(o.Destination) |> Option.ofObj
+            return {Property=o.Property; ElementName= destination.Element.Name.LocalName }}
+
+    let actionMap (vc:ProxiedViewController) (ac:ActionConnection) =
+        maybe {
+            let! destination = vc.FindById(ac.Destination) |> Option.ofObj
+            return {Selector=ac.Selector;ElementName= destination.Element.Name.LocalName}}
+
     let scenesFromXDoc (xdoc:XDocument) =
         let scenes =
             match Parser.Instance.Parse(xdoc.Root, DeviceFamily.Undefined) with
@@ -22,21 +32,22 @@ module IOS =
                         
                         let newOutlets =
                             outlets
-                            |> Seq.map (fun o -> {Property=o.Property; ElementName=o.Element.Name.LocalName } )
+                            |> Seq.choose (outletMap vc)
                             |> Seq.toList
-                            
-                        let actions = [
-                            match vc.View with
-                            | null -> ()
-                            | view ->
-                                match view.Subviews with
-                                | null -> ()
-                                | subviews ->
-                                    yield! subviews
-                                           |> Seq.collect (fun sv -> sv.Actions)
-                                           |> Seq.distinct
-                                           |> Seq.map (fun ac -> {Selector=ac.Selector;ElementName=ac.Element.Name.LocalName} ) ]
+
+                        let actions = maybe {
+                            let! view = vc.View |> Option.ofObj
+                            let! subviews = view.Subviews |> Option.ofObj
+                            return subviews
+                                   |> Seq.collect (fun sv -> sv.Actions)
+                                   |> Seq.distinct
+                                   |> Seq.choose (actionMap vc)
+                                   |> Seq.toList } |> Option.fill List.empty
                         
-                        let newVc = {XmlType=vc.Element.Name.LocalName; CustomClass=vc.CustomClass; Outlets = newOutlets; Actions = actions}
+                        let newVc = {XmlType     = vc.Element.Name.LocalName
+                                     CustomClass = vc.CustomClass
+                                     Outlets     = newOutlets
+                                     Actions     = actions}
+
                         let scene = {ViewController=newVc}
                         scene)
